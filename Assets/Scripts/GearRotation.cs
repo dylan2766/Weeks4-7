db3cd83af4a41fa3195936819d7602062ace455e
dylan2766 <reevesdy@sheridancollege.ca>
2025-02-05 17:34:25 -0500

    - Added gears - Changed speed

---
+++ Assets/Scripts/GearRotation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GearRotation : MonoBehaviour
+{
+    //Variables
+    public float rotSpeed;
+    public Slider sliderSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Gets value of slider to set as speed
+        rotSpeed = sliderSpeed.value;
+
+        Vector3 pos = transform.rotation.eulerAngles;
+        pos.z += (-rotSpeed * 15) * Time.deltaTime;
+        transform.eulerAngles = pos;
+    }
+}
